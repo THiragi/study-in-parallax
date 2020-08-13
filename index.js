d76f2931e7c0324312e534dd8b1303607f7fd87e
THiragi <w1th1nmtbr41n@gmail.com>
2020-08-13 18:20:44 +0900

    add requestAnimationFrame

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
+function parallaxBackground() {
   const targetFactor = 0.25; // パララックスの移動量
   const windowHeight = document.documentElement.clientHeight; // 画面の高さ
 
   const parallax = document.getElementsByClassName('parallax');
   const targets = Array.prototype.slice.call(parallax);
+  const scrollY = Math.max(window.pageYOffset, document.documentElement.scrollTop);
 
+  for (const target of targets) {
+    const targetOffsetTop = target.offsetTop;
+    const scrollYStart = targetOffsetTop - windowHeight;
 
-  window.addEventListener("scroll", () => {
-    const scrollY = Math.max(window.pageYOffset, document.documentElement.scrollTop);
-
-    for (const target of targets) {
-      const targetOffsetTop = target.offsetTop;
-      const scrollYStart = targetOffsetTop - windowHeight;
-
-      if(scrollY > scrollYStart){
-        target.style.backgroundPositionY = `${(scrollY - targetOffsetTop) * targetFactor}px`;
-      }else{
-        target.style.backgroundPosition ='center top';
-      }
+    if(scrollY > scrollYStart){
+      target.style.backgroundPositionY = `${(scrollY - targetOffsetTop) * targetFactor}px`;
+    }else{
+      target.style.backgroundPosition ='center top';
     }
-    
-  });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  requestAnimationFrame(parallaxBackground());
 });
 
-
+window.addEventListener("scroll", () => {
+  requestAnimationFrame(parallaxBackground());
+});
 
