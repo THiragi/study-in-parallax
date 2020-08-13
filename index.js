99cca7c700e4bd08b9557c013e3e520dd1e9bedd
THiragi <w1th1nmtbr41n@gmail.com>
2020-08-13 17:33:34 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const targetFactor = 0.5; // パララックスの移動量
+  const windowHeight = document.documentElement.clientHeight; // 画面の高さ
+
+  const parallax = document.getElementsByClassName('parallax');
+  const targets = Array.prototype.slice.call(parallax);
+
+
+  window.addEventListener("scroll", () => {
+    const scrollY = Math.max(window.pageYOffset, document.documentElement.scrollTop);
+
+    for (const target of targets) {
+      const targetOffsetTop = target.offsetTop;
+      const scrollYStart = targetOffsetTop - windowHeight;
+
+      if(scrollY > scrollYStart){
+        target.style.backgroundPositionY = `${(scrollY - targetOffsetTop) * targetFactor}px`;
+      }else{
+        target.style.backgroundPosition ='center top';
+      }
+    }
+    
+  });
+});
+
+
+
