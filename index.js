5fcc02775c816e1a88f0ec08fab37e9bb2765670
THiragi <w1th1nmtbr41n@gmail.com>
2020-08-19 23:11:46 +0900

    functional style

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
-// パララックスの移動量
-const targetFactor = 0.25;
-// 画面の縦幅
-const windowHeight = Math.min(window.innerHeight, document.documentElement.clientHeight); 
+const getParallaxStyle = factor =>　windowHeight => positionY => target => {
+  const targetOffsetTop = target.offsetTop;
+  const scrollYStart = targetOffsetTop - windowHeight;
+  return (positionY > scrollYStart) ? `${(positionY - targetOffsetTop) * factor}px` : 'top';
+};
 
-// パララックスを当てる要素のHTMLCollectionをクラス名で取得
-const parallax = document.getElementsByClassName('parallax');
-// HTMLCollectionをHTMLElementの配列に変更
-const targets = Array.prototype.slice.call(parallax);
+const setStyle = fn => target => target.style.backgroundPositionY = fn(target);
 
-// パララックス処理関数
-function parallaxBackground() {
-  // スクロールのY位置
-  const scrollY = Math.max(window.pageYOffset, document.documentElement.scrollTop);
+window.onload = () => {
+  const parallax = document.getElementsByClassName('parallax');
+  const targets = Array.prototype.slice.call(parallax);
 
-  // 配列に入っている全てのHTMLElementにパララックス処理をあてる
-  for (const target of targets) {
-    // 対象要素のdocument内での位置
-    const targetOffsetTop = target.offsetTop;
-    // パララックス処理をかけるスクロール位置
-    const scrollYStart = targetOffsetTop - windowHeight;
-    // 所定のスクロール位置に達したら、対象要素の背景画像にスクロール位置から算出した値をあてる。
-    target.style.backgroundPositionY = (scrollY > scrollYStart) ? `${(scrollY - targetOffsetTop) * targetFactor}px` : 'top';
-  }
-}
+  const windowHeight = Math.min(window.innerHeight, document.documentElement.clientHeight);
+  const parallaxCond = getParallaxStyle(0.25)(windowHeight);
+  
+  const loadStyle = parallaxCond(Math.max(window.pageYOffset, document.documentElement.scrollTop));
+  targets.forEach(setStyle(loadStyle));
 
-// 画面読み込み時に一度パララックス処理をあてる
-// 画面読み込み時に背景画像が一枚しか表示されないなら不要
-document.addEventListener('DOMContentLoaded', () => {
-  requestAnimationFrame(parallaxBackground);
-});
-
-// スクロールイベントにパララックス処理をあてる
-window.addEventListener("scroll", () => {
-  requestAnimationFrame(parallaxBackground);
-});
-
+  window.addEventListener("scroll", () => {
+    const scrollStyle = parallaxCond(Math.max(window.pageYOffset, document.documentElement.scrollTop));
+    targets.forEach(setStyle(scrollStyle));
+  });
+};
